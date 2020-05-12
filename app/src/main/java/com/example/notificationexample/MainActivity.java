019da9f3b14774867ad5a38131469640d7334f32
Michael Gaudreau <gaudreaum01@gmail.com>
2020-05-12 22:10:35 +0800

    Add necessary imports to MainActivity.java file for notifications

--- app/src/main/java/com/example/notificationexample/MainActivity.java
+++ app/src/main/java/com/example/notificationexample/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.notificationexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static com.example.notificationexample.App.CHANNEL_1_ID;
 
 public class MainActivity extends AppCompatActivity {
 
